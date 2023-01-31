1066b629f490814eeba0e2c1232afaa2811252c8
voronov-nikita <114691383+voronov-nikita@users.noreply.github.com>
2023-01-31 23:21:44 +0300

    sketch_future_app

--- MainApp.py
+++ MainApp.py
@@ -1,23 +1,81 @@
+import requests
+
 # Основное приложение
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.image import Image
 
 
 class MainScreen(Screen):
     def __init__(self):
         super().__init__()
         self.name = "Main"
-        self.fl = FloatLayout()
+        self.k = 1
+        res = requests.get(f"https://dt.miet.ru/ppo_it/api/temp_hum/{self.k}")
+        self.lbl = Label(text=res.text)
+        self.fl = BoxLayout(orientation="vertical")
+        self.Init()
+
+    def Init(self):
+        btn1 = Button(text="update",
+                      # size_hint=(.1, .1),
+                      # pos=(.5, .5),
+                      # pos_hint={'x': 0, 'y': 0.9},
+                      on_press=self.update
+                      )
+        btn2 = Button(text="next",
+                      # size_hint=(.1, .1),
+                      # pos=(.5, .5),
+                      # pos_hint={'x': 0.4, 'y': 0.9},
+                      on_press=self.count
+                      )
+        btn3 = Button(text="3",
+                      # size_hint=(.1, .1),
+                      # pos=(.5, .5),
+                      # pos_hint={'x': 0.8, 'y': 0.9},
+                      on_press=self.next
+                      )
+        self.fl.add_widget(self.lbl)
+        self.fl.add_widget(btn1)
+        self.fl.add_widget(btn2)
+        self.fl.add_widget(btn3)
+
+        self.add_widget(self.fl)
+
+    def next(self, instance):
+        self.manager.transition.direction = 'right'
+        self.manager.current = "Second"
+        return 0
+
+    def update(self, instance):
+        res = requests.get(f"https://dt.miet.ru/ppo_it/api/temp_hum/{self.k}")
+        self.lbl.text = res.text
+
+    def count(self, instance):
+        if self.k == 4:
+            self.k = 1
+        else:
+            self.k += 1
+        res = requests.get(f"https://dt.miet.ru/ppo_it/api/temp_hum/{self.k}")
+        self.lbl.text = res.text
+
+
+class SecondScreen(Screen):
+    def __init__(self):
+        super().__init__()
+        self.name = "Second"
+        self.fl = BoxLayout()
         self.Init()
 
     def Init(self):
         btn1 = Button(text="1",
                       # size_hint=(.1, .1),
-                      pos=(.5, .5),
-                      pos_hint={'x': 0, 'y': 0.9},
+                      # pos=(.5, .5),
+                      # pos_hint={'x': 0, 'y': 0.9},
                       on_press=self.next
                       )
         btn2 = Button(text="2",
@@ -30,44 +88,8 @@ class MainScreen(Screen):
                       # size_hint=(.1, .1),
                       # pos=(.5, .5),
                       # pos_hint={'x': 0.8, 'y': 0.9},
-                      on_press=self.next
-                      )
-        self.fl.add_widget(btn1)
-        self.fl.add_widget(btn2)
-        self.fl.add_widget(btn3)
-
-        self.add_widget(self.fl)
-
-    def next(self, instance):
-        self.manager.transition.direction = 'right'
-        self.manager.current = "Second"
-        return 0
-
-
-class SecondScreen(Screen):
-    def __init__(self):
-        super().__init__()
-        self.name = "Second"
-        self.fl = FloatLayout()
-        self.Init()
-
-    def Init(self):
-        btn1 = Button(text = "1",
-                      # size_hint=(.9, .1),
-                      # pos=(.5, .5),
-                      # pos_hint={'x': 0, 'y': 0},
-                      on_press=self.next
-                      )
-        btn2 = Button(text = "2",
-                      # size_hint=(.1, .1),
-                      # pos=(.5, .5),
-                      # pos_hint={'x': 0.4, 'y': 0},
-                      on_press=self.next
-                      )
-        btn3 = Button(text = "3",
-                      # size_hint=(.1, .1),
-                      # pos=(.5, .5),
-                      # pos_hint={'x': 0.8, 'y': 0},
+                      background_color=(255, 255, 255),
+                      color="red",
                       on_press=self.next
                       )
         self.fl.add_widget(btn1)
