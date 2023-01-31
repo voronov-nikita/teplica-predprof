61f1840e84c5187d3be08d6ad28b8feac7ce071b
voronov-nikita <114691383+voronov-nikita@users.noreply.github.com>
2023-01-31 20:49:47 +0300

    no_new

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
+# Просьба коментировать весь написанный код, если же комент требуется!
+
+
+# Для отображения информации
 import matplotlib as mt
 import requests
 
-from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix
+# Основное приложение
+# from kivy.app import App
+# from kivy.uix.label import Label
+# from kivy.uix.button import Button
+# from kivy.uix.floatlayout import FloatLayout
+
+# сайт на который отправляем GET запрос
+# Пример!
+# типо опрашиваем сервер
+list_data_info=[]
+for i in range(1, 5):
+    res = requests.get(f'https://dt.miet.ru/ppo_it/api/temp_hum/{i}')
+    list_data_info.append(res.text)
 
 
-res = requests.get('https://dt.miet.ru/ppo_it/api/temp_hum/2')
 
-ls = res.text.split(",")
 
-for i in ls:
-    print(i)
