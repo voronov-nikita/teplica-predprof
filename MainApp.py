e4c714cc1c7a7ad391d94f47b90d582225613bdf
voronov-nikita <114691383+voronov-nikita@users.noreply.github.com>
2023-01-31 23:27:29 +0300

    update_sketch

--- MainApp.py
+++ MainApp.py
@@ -33,7 +33,7 @@ class MainScreen(Screen):
                       # pos_hint={'x': 0.4, 'y': 0.9},
                       on_press=self.count
                       )
-        btn3 = Button(text="3",
+        btn3 = Button(text="humidity",
                       # size_hint=(.1, .1),
                       # pos=(.5, .5),
                       # pos_hint={'x': 0.8, 'y': 0.9},
@@ -68,30 +68,32 @@ class SecondScreen(Screen):
     def __init__(self):
         super().__init__()
         self.name = "Second"
-        self.fl = BoxLayout()
+        self.k = 1
+        res = requests.get(f'https://dt.miet.ru/ppo_it/api/hum/{self.k}')
+        self.lbl = Label(text = res.text)
+        self.fl = BoxLayout(orientation = 'vertical')
         self.Init()
 
     def Init(self):
-        btn1 = Button(text="1",
+        btn1 = Button(text="update",
                       # size_hint=(.1, .1),
                       # pos=(.5, .5),
                       # pos_hint={'x': 0, 'y': 0.9},
-                      on_press=self.next
+                      on_press=self.update
                       )
-        btn2 = Button(text="2",
+        btn2 = Button(text="next",
                       # size_hint=(.1, .1),
                       # pos=(.5, .5),
                       # pos_hint={'x': 0.4, 'y': 0.9},
-                      on_press=self.next
+                      on_press=self.count
                       )
-        btn3 = Button(text="3",
+        btn3 = Button(text="temperature",
                       # size_hint=(.1, .1),
                       # pos=(.5, .5),
                       # pos_hint={'x': 0.8, 'y': 0.9},
-                      background_color=(255, 255, 255),
-                      color="red",
                       on_press=self.next
                       )
+        self.fl.add_widget(self.lbl)
         self.fl.add_widget(btn1)
         self.fl.add_widget(btn2)
         self.fl.add_widget(btn3)
@@ -102,6 +104,18 @@ class SecondScreen(Screen):
         self.manager.transition.direction = 'left'
         self.manager.current = "Main"
         return 0
+
+    def update(self, instance):
+        res = requests.get(f'https://dt.miet.ru/ppo_it/api/hum/{self.k}')
+        self.lbl.text = res.text
+
+    def count(self, instance):
+        if self.k == 6:
+            self.k = 1
+        else:
+            self.k += 1
+        res = requests.get(f'https://dt.miet.ru/ppo_it/api/hum/{self.k}')
+        self.lbl.text = res.text
 
 
 class MainApp(App):
