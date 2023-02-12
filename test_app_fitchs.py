93cf27062a767b4cf861f4f6dec207a3be6f1114
voronov-nikita <114691383+voronov-nikita@users.noreply.github.com>
2023-02-12 16:01:07 +0300

    patch_value

--- test_app_fitchs.py
+++ test_app_fitchs.py
@@ -1,9 +1,15 @@
 import requests
 import json
 
-ls = json.loads(str(
-    requests.get(f"https://dt.miet.ru/ppo_it/api/temp_hum/1").text
-))
-print(ls)
-print(ls["id"])
-print(ls["temperature"])
+dicti = {
+    "state": 0
+}
+
+# патч запросы!!!
+res1 = requests.patch("https://dt.miet.ru/ppo_it/api/fork_drive", params={"state": 0})
+res2 = requests.patch("https://dt.miet.ru/ppo_it/api/total_hum", params={"state":0})
+res3 = requests.patch("https://dt.miet.ru/ppo_it/api/watering", params={"id":6, "state":0})
+
+print(res1.status_code)
+print(res2.status_code)
+print(res3.status_code)
