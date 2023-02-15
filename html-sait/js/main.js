3a9acf0bf94f607be8b41a00ad072396611906ea
DimdonTM <74155113+DimdonTM@users.noreply.github.com>
2023-02-15 18:24:41 +0300

    Update js file

--- html-sait/js/main.js
+++ html-sait/js/main.js
@@ -1,7 +1,42 @@
-var typed = new Typed(".mov",{
-    strings: [, "We welcome you, dear visitors of our site. On this page you will learn how to install our application. <br> So, to download the application, we need to go to the HOME tab,<br> Then click on the INSTALL button and the application will start downloading. <br> Next, we go to the browser's download history and install an application called: TPGH. [In the future, an installation video will be attached below]"] ,
-    startDelay: (500),
-    backDelay: (99999999),
-    typeSpeed: 25,
-    loop: true,
-    })
+function onEntryText(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+        if (k==0){
+        var typed = new Typed(".mov",{
+            strings: [, "We welcome you, dear visitors of our site. On this page you will learn how to install our application. <br> So, to download the application, we need to go to the HOME tab,<br> Then click on the INSTALL button and the application will start downloading. <br> Next, we go to the browser's download history and install an application called: GreenHouse."] ,
+            startDelay: (500),
+            backDelay: (99999999),
+            typeSpeed: 25,
+            loop: true,
+            })
+        k = 1;
+      }}
+    });
+  }
+  
+  let optionstext = {
+    threshold: [0.5] };
+  let observertext = new IntersectionObserver(onEntryText, optionstext);
+  let elementstext = document.querySelectorAll('.for__what2');
+  let k = 0;
+  for (let elmtext of elementstext) {
+    observertext.observe(elmtext);
+  }
+
+
+  function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+       change.target.classList.add('element-show');
+      }
+    });
+  }
+  
+  let options = {
+    threshold: [0.5] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('.section__title_installion');
+  
+  for (let elm of elements) {
+    observer.observe(elm);
+  }
