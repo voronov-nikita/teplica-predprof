68a3e7f6240f9949ec06dbbab4d0c5701301cdcf
DimdonTM <74155113+DimdonTM@users.noreply.github.com>
2023-02-20 10:03:33 +0300

    fix

--- html-sait/js/main.js
+++ html-sait/js/main.js
@@ -1,9 +1,9 @@
-sfunction onEntryText(entry) {
+function onEntryText(entry) {
     entry.forEach(change => {
       if (change.isIntersecting) {
         if (k==0){
         var typed = new Typed(".mov",{
-            strings: [, "We welcome you, dear visitors of our site. On this page you will learn how to install our application. <br> So, to download the application, we need to go to the HOME tab,<br> Then click on the INSTALL button and the application will start downloading. <br> Next, we go to the browser's download history and install an application called: GreenHouse."] ,
+            strings: [, "We welcome you, dear visitors of our site. On this page you will learn how to install our application. So, to download the application, we need to go to the HOME tab, Then click on the INSTALL button and the application will start downloading. Next, we go to the browser's download history and install an application called: GreenHouse."] ,
             startDelay: (500),
             backDelay: (99999999),
             typeSpeed: 25,
@@ -35,7 +35,7 @@ sfunction onEntryText(entry) {
   let options = {
     threshold: [0.5] };
   let observer = new IntersectionObserver(onEntry, options);
-  let elements = document.querySelectorAll('.section__title_installion');
+  let elements = document.querySelectorAll('.installing_show');
   
   for (let elm of elements) {
     observer.observe(elm);
